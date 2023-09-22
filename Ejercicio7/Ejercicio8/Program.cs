8839dc3b88a1872e1e858d4e1cd435f933c0e6d9
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-09-22 12:54:45 -0500

    EJercicio 8

---
+++ Ejercicio7/Ejercicio8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio8
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            bool EsPar = false;
+
+            Console.WriteLine("Ingrese numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            { 
+                EsPar = true;
+            }            
+            if (EsPar)
+            {
+                Console.WriteLine($"El numero {num1} es PAR");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num1} es IMPAR");
+            }
+        }
+    }
+}
